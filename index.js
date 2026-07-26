7bb5414d9ed47640c6706c54a66845f905a34d5c
rawad3423-dev <rawad3423@gmail.com>
2026-07-26 16:10:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
+
+const ROLE_ID = "1530910734886764705";
+
+client.on("guildMemberUpdate", async (oldMember, newMember) => {
+  if (
+    !oldMember.roles.cache.has(ROLE_ID) &&
+    newMember.roles.cache.has(ROLE_ID)
+  ) {
+    try {
+      await newMember.ban({ reason: "تم أخذ الرول الممنوع" });
+      console.log(`${newMember.user.tag} تم حظره.`);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+});
+
+client.once("ready", () => {
+  console.log(`تم تشغيل ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
